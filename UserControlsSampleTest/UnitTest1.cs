6b9af0ef0582592885e5449865334d093783cc13
popcorn <popcorn088@gmail.com>
2024-12-26 02:32:21 +0900

    UnitTest追加

---
+++ UserControlsSampleTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using UserControlsSample.CoefsTableSample;
+
+namespace UserControlsSampleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        CoefsTablePageViewModel _vm;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _vm = new CoefsTablePageViewModel();
+        }
+    }
+}
